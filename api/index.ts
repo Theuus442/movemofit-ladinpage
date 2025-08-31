5a1d91790101fa7aa55e9e9dba001c8c764c2ac2
Matheus-Alves <160160539+Theuus442@users.noreply.github.com>
2025-08-31 01:26:39 -0300

    Add files via upload

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
-import serverless from "serverless-http";
 import { createServer } from "../server";
 
-// Wrap our Express app into a Vercel serverless function
 const app = createServer();
-export default serverless(app);
+
+export default function handler(req: unknown, res: unknown) {
+  return (app as any)(req as any, res as any);
+}
